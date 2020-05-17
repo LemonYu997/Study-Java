a343bede43834927522760c9e72391cb7b06da1d
LeminYu997 <1445531395@qq.com>
2020-05-17 22:15:15 +0800

    new file

---
+++ chapter3/enums/EnumTest.java
@@ -0,0 +1,38 @@
+//3.5 枚举类  enums   这是个类
+package chapter3.enums;
+
+import java.util.Scanner;
+
+public class EnumTest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a size: (SMALL, MEDIUM, LARGE, EXTRA_LARGE) ");
+        String input = in.next().toUpperCase();         //将输入的单词转为大写
+
+        Size size = Enum.valueOf(Size.class, input);
+        System.out.println("size=" + size);
+        System.out.println("abbreviation=" + size.getAbbreviation());
+        if (size == Size.EXTRA_LARGE)
+            System.out.println("Good job--you paid attention to the _.");
+    }
+}
+
+//创建枚举类
+enum Size
+{
+    SMALL("S"), MEDIU("M"), LARGE("L"), EXTRA_LARGE("XL");
+
+    private Size(String abbreviation)
+    {
+        this.abbreviation = abbreviation;
+    }
+
+    public String getAbbreviation()
+    {
+        return abbreviation;
+    }
+
+    //缩写
+    private String abbreviation;
+}
